8884381851c2c3afe80966fea3ff783f94d2d8ba
Eriuden <antoinecarrette@hotmail.fr>
2023-09-02 10:44:12 +0200

    forget some code

--- src/components/NavBook.js
+++ src/components/NavBook.js
@@ -1,11 +1,26 @@
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Squash as Hamburger } from "hamburger-react"
 import Github from "../img/GitHub-Logo.png"
 import Linkedin from "../img/LinkedIn-Symbole.png"
 
 export const NavBook = () => {
     const [hamburger, setHamburger] = useState(false)
+    const [navSize, setNavSize] = useState(0)
+
+    useEffect(() => {
+        if (hamburger) {
+          const timer = setTimeout(() => {
+            setNavSize(45);
+          }, 75);
+          
+          return () => clearTimeout(timer);
+        } else {
+          
+          setNavSize(0);
+        }
+      }, [hamburger]);
+
   return (
     <div className='sticky top-0'>
         <h2 className='flex m-3' onClick={()=> setHamburger (!hamburger)}>
@@ -16,7 +31,9 @@ export const NavBook = () => {
              
             <nav className='flex flex-col items-start justify-start 
             ml-3.5 absolute opacity-100
-             md:max-w-[55%] xl:max-w-[45%]  navBook'>
+             md:max-w-[55%] xl:max-w-[45%]  navBook'
+             style={{ width: `${navSize}%`, transition: 'width 0.3s ease-in-out' }}
+            >
                 <ul className=' text-xs sm:text-xs md:text-md lg:text-lg xl:text-xl '>
                     <li className='flex flex-row my-1   '>
                         <h3 className='mx-4 mt-4 sm:mb-2  '>M'envoyer un Hibou</h3>
@@ -24,14 +41,12 @@ export const NavBook = () => {
                     </li>
 
                     
-                    
                     <li className='flex flex-row my-2 '>
                         <h3 className='mx-6 sm:mt-4 sm:mb-4 '>Appel télépathique</h3>
                         <br/>
                         <p className=' phone ml-[8%] sm:ml-[20%] md:ml-[17%] lg:ml-[12%] 2xl:ml-[15%] sm:mt-4 sm:mb-2  '>06.24.66.24.51</p>
                     </li>
-
-                    
+             
 
                     <li className='flex flex-row my-2'>
                         <h3 className='ml-6 sm:mt-4 sm:mb-4 '>Chambre du conseil</h3>
@@ -39,8 +54,7 @@ export const NavBook = () => {
                             <img src={Linkedin} alt="linkedin" className="xs:ml-[20%] sm:ml-[24%] md:ml-[28%] lg:ml-[28%] 2xl:ml-[25%] max-w-[30%] max-h-[30%] mt-4 mb-4  "/>
 		                </a>
                     </li>
-
-                   
+              
 
                     <li className='flex flex-row my-2'>
                         <h3 className='mx-6 sm:mt-4 sm:mb-4 md:mt-2 md:mb-2'>Ma tour</h3>
